54939d35579c585ef0e8174e6455df2f09e306f2
pareshpatel <pareshpatel75@gmail.com>
2022-06-01 15:58:23 +0100

    seleniumhomework

---
+++ SeleEdge.java
@@ -0,0 +1,20 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class SeleEdge {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+        Thread.sleep(2000);
+        driver.findElement(By.className("ico-login")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("Email")).sendKeys("tester@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("tester01");
+        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
+
+    }
+}
---
+++ SeleFirefox.java
@@ -0,0 +1,22 @@
+package SeleLerning;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class SeleFirefox {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demo.nopcommerce.com/");
+        Thread.sleep(2000);
+        driver.findElement(By.className("ico-login")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("Email")).sendKeys("tester@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("tester01");
+        driver.findElement(By.xpath("/html/body/div[6]/div[3]/div/div/div/div[2]/div[1]/div[2]/form/div[3]/button")).click();
+
+    }
+}
